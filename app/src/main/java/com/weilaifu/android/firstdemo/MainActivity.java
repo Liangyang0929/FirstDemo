fd04c4021ce08fe9edda0aae1a6f592e950b63ee
Liang Yang <liangyang0929@gmail.com>
2017-06-21 11:14:16 +0800

    clear

--- app/src/main/java/com/weilaifu/android/firstdemo/MainActivity.java
+++ app/src/main/java/com/weilaifu/android/firstdemo/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 int i=0;
+
+                String a="a";
             }
         });
     }
